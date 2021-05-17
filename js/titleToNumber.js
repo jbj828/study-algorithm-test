1618b7057cc6fc54876ff3a9fe388580bf828482
Chung Byung Jae <jbj828@naver.com>
2021-05-17 21:27:19 +0900

    study algorithm

---
+++ js/titleToNumber.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function (columnTitle) {
+  /*
+        length가 2면 26 * B(2), 26 * C(3), 
+        첫번째 자리는 a,b,c에 해당하는 1,2,3... 더 하기
+        BCD : 26 * 26 * B(2) + (26 * C(3)) + D(4);
+    */
+};
