bffc3a0c19280a7818b852b6bfb7167c564981d3
Chung Byung Jae <jbj828@naver.com>
2021-08-02 00:33:29 +0900

    study algorithm

---
+++ cpp/hackerRank/swapPairs.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <iostream>
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* swapPairs(ListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+
+    ListNode* curr = head;
+    ListNode* prev = nullptr;
+    while (curr->next != nullptr) {
+      if (prev == nullptr) {
+        head = curr->next;
+      } else {
+        prev->next = curr->next;
+      }
+
+      ListNode* after = curr->next;
+      curr->next = curr->next->next;
+      after->next = curr;
+
+      if (curr->next == nullptr) {
+        break;
+      }
+      prev = curr;
+      curr = curr->next;
+    }
+
+    return head;
+  }
+};
