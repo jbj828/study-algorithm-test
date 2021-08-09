18446395a65cb1ab238c233adefebb2122a4f5c0
Chung Byung Jae <jbj828@naver.com>
2021-08-09 14:58:38 +0900

    study algorithm

--- cpp/leetcode/reverseKGroup.cpp
+++ cpp/leetcode/reverseKGroup.cpp
@@ -8,6 +8,16 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+#include <iostream>
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
 class Solution {
  public:
   ListNode* reverseKGroup(ListNode* head, int k) {
