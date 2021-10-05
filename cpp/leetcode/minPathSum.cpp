b4ac123a501983f3347558c6e982cab6f58fe54a
Chung Byung Jae <jbj828@naver.com>
2021-10-05 22:47:34 +0900

    study algorithm

--- cpp/leetcode/minPathSum.cpp
+++ cpp/leetcode/minPathSum.cpp
@@ -5,25 +5,28 @@ using namespace std;
 
 class Solution {
  public:
-  vector<vector<int> > cache;
-  int sum = 9000000;
-  int helper(int x, int y, int m, int n, const vector<vector<int> >& grid) {
+  int helper(int x, int y, int m, int n, const vector<vector<int> >& grid,
+             vector<vector<int> >& cache) {
     // base case
     if (x == m - 1 && y == n - 1) return grid[y][x];
 
-    if (x >= m || y >= n) return 0;
+    if (x >= m || y >= n) {
+      return 900000;
+    }
+
+    if (cache[y][x] != -1) return cache[y][x];
 
     // 조각
-    // 옆 이동
-    int go_next = grid[y][x] + helper(x + 1, y, m, n, grid);
-    int go_bottom = grid[y][x] + helper(x, y + 1, m, n, grid);
+    int go_next = grid[y][x] + helper(x + 1, y, m, n, grid, cache);
+    int go_bottom = grid[y][x] + helper(x, y + 1, m, n, grid, cache);
 
-    return min(go_next, go_bottom);
+    return cache[y][x] = min(go_next, go_bottom);
   }
 
   int minPathSum(vector<vector<int> >& grid) {
     int n = grid.size();
     int m = grid[0].size();
-    return helper(0, 0, m, n, grid);
+    vector<vector<int> > cache(n, vector<int>(m, -1));
+    return helper(0, 0, m, n, grid, cache);
   }
 };
